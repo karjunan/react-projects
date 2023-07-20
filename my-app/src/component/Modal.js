90a4fdd279afdcb7d326323e12157056d393cd92
DBiz Factweavers <dbizfactweavers@DBizs-MacBook-Pro.local>
2023-07-20 20:37:38 +0530

    meet-up application

--- my-app/src/component/Modal.js
+++ my-app/src/component/Modal.js
@@ -1,9 +1,9 @@
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
       <p> Are you Sure ? </p>
-      <button className="btn btn--alt">Cancel</button>
-      <button className="btn">Submit</button>
+      <button className="btn btn--alt" onClick={props.onClick}>Cancel</button>
+      <button className="btn" onClick={props.onClick}>Submit</button>
     </div>
   );
 }
--- my-app/src/component/Todo.js
+++ my-app/src/component/Todo.js
@@ -12,14 +12,19 @@ function deleteHandler() {
     console.log("Clicked", props.text);
     setModalIsOpen(true);
 }
+
+function cancelHandler() {
+  setModalIsOpen(false);
+}
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
         <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
-      {showModal ? <Modal /> : null }
-      {showModal ? <Backdrop /> : null }
+      {showModal ? <Modal onClick={cancelHandler}/> : null }
+      {showModal ? <Backdrop onClick={cancelHandler}/> : null }
     </div>
   );
 }
